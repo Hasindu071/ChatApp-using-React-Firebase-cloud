8aabcead6e6cc2581eff573333fd4a89bc9a76ea
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2025-01-25 16:07:33 +0530

    commets added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React, { useState, useEffect } from "react";
 import { auth } from "./components/firebase/firebase";
 import { onAuthStateChanged } from "firebase/auth";
-import SignUpForm from "./components/Auth/signUpForm";
 import SignInForm from "./components/Auth/signInForm";
 import SignOutButton from "./components/Auth/SignOutButton";
 
+
 const App = () => {
   const [user, setUser] = useState(null);
+  //state variable which saves the currunt authenticated user
+  //if user is not authenticated it is null
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -20,14 +22,15 @@ const App = () => {
     <div>
       {user ? (
         <div>
+        {/* if the user is authenticated */}
           <h1>Welcome, {user.email}</h1>
           <SignOutButton />
         </div>
       ) : (
         <div>
+        {/* if the user is not authenticated */}
           <h1>Authentication</h1>
           <SignInForm />
-          <SignUpForm />
         </div>
       )}
     </div>
